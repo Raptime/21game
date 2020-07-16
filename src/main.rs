ef3a183756c1ac4a3d09cdaf1c35d3138983687f
Raptime <raptime@users.noreply.github.com>
2020-07-15 20:58:33 -0400

    code cleanup

--- src/main.rs
+++ src/main.rs
@@ -6,10 +6,8 @@ fn main() {
     let mut cpu;
 
     loop {
-        println!(
-            "Choose your number: (1, 2, 3 or q to quit), running total is: {}",
-            run_tot
-        );
+        println!("Current total: {}", run_tot);
+        println!("Choose your number: (1, 2, 3 or q to quit)");
 
         let mut input = String::new();
 
@@ -18,7 +16,6 @@ fn main() {
             .expect("Failed to read line");
 
         if input.trim() == "q" {
-            println!("quiting...");
             break;
         }
 
@@ -28,13 +25,7 @@ fn main() {
         };
 
         match input {
-            1 | 2 | 3 => {
-                run_tot += input;
-                println!(
-                    "adding {} to running total...\nnew total is: {}",
-                    input, run_tot
-                );
-            }
+            1 | 2 | 3 => run_tot += input,
             _ => println!("you can only add 1, 2 or 3 to the total!"),
         }
 
@@ -45,24 +36,17 @@ fn main() {
 
         if run_tot < 18 {
             cpu = rand::thread_rng().gen_range(1, 3);
-            run_tot += cpu;
-
-            println!(
-                "Computer is adding {} to running total\nnew total is: {}",
-                cpu, run_tot
-            );
         } else {
             cpu = 21 - run_tot;
-            run_tot += cpu;
-            println!(
-                "Computer is adding {} to running total\nnew total is: {}",
-                cpu, run_tot
-            );
         }
+
+        run_tot += cpu;
+        println!("Computer is adding {}", cpu);
 
         if run_tot == 21 {
             println!("Game is over! Too bad, CPU wins!");
             break;
         }
     }
+    println!("quiting...");
 }
